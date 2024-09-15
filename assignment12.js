eea9ef8043319ec264296ff4c55ddb487568e681
samim <samimsaleklotfi@gmail.com>
2024-09-15 01:18:42 -0800

    the first and the second ones were done

---
+++ assignment12.js
@@ -0,0 +1,15 @@
+// 1. Write a function `greet(name)` that takes a person's name as a parameter and
+// returns a greeting like `"Hello, John!"`. Test it with the name `"Alice"`.
+function greet(name) {
+  console.log(`"Hello ${name}"`);
+}
+greet("alice");
+
+// 2. Create an object `dog` with properties `name` and `breed`. Display the `name`
+// and `breed` of the dog in the console.
+var dog = { name: "popy",
+    breed:"american"
+ };
+ console.log(" the dog name is "+ dog.name + " and the breed of dog is "+dog.breed );
+ 
+ 
